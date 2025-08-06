17d76c11603f19fbe8f463ff3047797a53ab7d24
Henrique Nogara <nogara.henrique@gmail.com>
2025-08-06 10:57:01 -0300

    Add PolymeshHooks to contracts

--- substrate/frame/contracts/src/exec.rs
+++ substrate/frame/contracts/src/exec.rs
@@ -965,6 +965,11 @@ where
 				// Root origin can't be used to instantiate a contract, so it is safe to assume that
 				// if we reached this point the origin has an associated account.
 				let origin = &self.origin.account_id()?;
+				// Polymesh change
+				// -----------------------------------------------------------------
+				let top_frame = self.top_frame();
+				<Contracts<T>>::on_instantiate_transfer(origin, &top_frame.account_id)?;
+				// -----------------------------------------------------------------
 				let frame = top_frame_mut!(self);
 				frame.nested_storage.charge_instantiate(
 					origin,
