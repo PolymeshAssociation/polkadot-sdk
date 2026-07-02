063bca56e0adbbaad735e28e7c937ccc81fafe88
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2026-07-03 02:00:49 +0800

    Fix build of revive-dev-node.

--- substrate/frame/contracts/mock-network/src/parachain.rs
+++ substrate/frame/contracts/mock-network/src/parachain.rs
@@ -97,6 +97,7 @@ impl pallet_balances::Config for Runtime {
 	type RuntimeFreezeReason = RuntimeFreezeReason;
 	type WeightInfo = ();
 	type DoneSlashHandler = ();
+	type Memo = ();
 }
 
 parameter_types! {
